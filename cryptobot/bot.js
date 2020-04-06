614f2912a0cbcf9d9b58c218d0ea91db39c37c34
AlexandrMay <maysalexandr@gmail.com>
2020-04-06 21:11:33 +0300

    crypto bot inline keyboard finished

--- cryptobot/bot.js
+++ cryptobot/bot.js
@@ -2,45 +2,85 @@ const Telegraf = require('telegraf');
 const axios = require('axios');
 const bot = new Telegraf('1121680747:AAEqfUNd8T1b6az4f7O036BYRzkcEDGCbPc');
 
-bot.command('test', ctx => {
-    bot.telegram.sendMessage(ctx.chat.id, 'Main menu', {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {text: 'See Fruits list', callback_data: 'fruits'},
-                    {text: 'See Meats list', callback_data: 'meats'}
-                ]
-            ]
-        }
-    })
+const apiKey = "2e569498edb9a98b9475eb2d01b6bf1b7bfb5f3b25a517d56c219a8c1a9e575c";
 
+function sendStartMessage(ctx){
+    let startMessage = `Welcome, this bot gives you cryptocurrency information`;
+    bot.telegram.sendMessage(ctx.chat.id, startMessage,
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {text: 'Crypto Prices', callback_data: 'price'}
+                    ],
+                    [
+                        {text: 'CoinMarketCap', url: 'https://coinmarketcap.com/'}
+                    ],
+                ]
+            }
+        });
+}
+
+bot.command('start', ctx => {
+    sendStartMessage(ctx);
 });
 
-bot.action('fruits', ctx => {
+bot.action('start', ctx => {
     ctx.deleteMessage();
-    bot.telegram.sendMessage(ctx.chat.id, 'List of fruits: \n-Apples\n-Oranges\n-Pears', {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {text: 'Back to menu', callback_data: 'menu'}
-                ]
-            ]
-        }
-    })
+    sendStartMessage(ctx);
 });
 
-bot.action('menu', ctx => {
+bot.action('price', ctx => {
+    let priceMessage = `Get Price Information. Select any currency below.`;
     ctx.deleteMessage();
-    bot.telegram.sendMessage(ctx.chat.id, 'Main menu', {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {text: 'See Fruits list', callback_data: 'fruits'},
-                    {text: 'See Meats list', callback_data: 'meats'}
+    bot.telegram.sendMessage(ctx.chat.id, priceMessage,
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {text: 'BTC', callback_data: 'price-BTC'},
+                        {text: 'ETH', callback_data: 'price-ETH'}
+                    ],
+                    [
+                        {text: 'BCH', callback_data: 'price-BCH'},
+                        {text: 'LTC', callback_data: 'price-LTC'}
+                    ],
+                    [
+                        {text: 'Back to Menu', callback_data: 'start'}
+                    ]
                 ]
-            ]
-        }
-    })
+            }
+        })
+});
+
+let priceActionList = ['price-BTC', 'price-ETH', 'price-BCH', 'price-LTC'];
+bot.action(priceActionList, async ctx => {
+    let symbol = ctx.match.split('-')[1];
+    try {
+        let res = await axios.get(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${symbol}&tsyms=USD&api_key=${apiKey}`);
+        let data = res.data.DISPLAY[symbol].USD;
+        let message = `
+        Symbol: ${symbol}
+        Price: ${data.PRICE}
+        Open: ${data.OPENDAY}
+        High: ${data.HIGHDAY}
+        Low: ${data.LOWDAY}
+        Supply: ${data.SUPPLY}
+        Market Cap: ${data.MKTCAP}
+        `;
+        ctx.deleteMessage();
+        bot.telegram.sendMessage(ctx.chat.id, message, {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {text: 'Back to prices', callback_data: 'price'}
+                    ]
+                ]
+            }
+        })
+    } catch (e) {
+        console.log(e)
+    }
 });
 
 bot.launch();
